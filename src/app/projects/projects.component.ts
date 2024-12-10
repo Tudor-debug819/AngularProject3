85ed3f00c652bbc367911a6b10ee7d736cb47ea6
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-10 18:34:50 +0200

    cards almost finished

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -13,7 +13,7 @@ export class ProjectsComponent {
     {
       "title": "Comming Soon",
       "description": "I made a coming soon page for my main website which is responsive using media queries.",
-      "image": "imagini/coming-soon-photo.png",
+      "image": "assets/coming-soon-photo.png",
       "technologies": ["html", "css"],
       "githubLink": "https://github.com/Tudor-debug819/comingsoon",
       "liveLink": "https://tudor-debug819.github.io/comingsoon/"
@@ -21,7 +21,7 @@ export class ProjectsComponent {
     {
       "title": "Back to Top",
       "description": "A back to top page with a sticky arrow button that returns the user back to the top.",
-      "image": "imagini/back-to-top-photo.png",
+      "image": "assets/back-to-top-photo.png",
       "technologies": ["html", "css"],
       "githubLink": "https://github.com/Tudor-debug819/backtotop",
       "liveLink": "https://tudor-debug819.github.io/backtotop/"
@@ -29,7 +29,7 @@ export class ProjectsComponent {
     {
       "title": "Site layout",
       "description": "Two web pages created with grid and flexbox layouts to improve understanding.",
-      "image": "imagini/site-layout-photo.png",
+      "image": "assets/site-layout-photo.png",
       "technologies": ["html", "css"],
       "githubLink": "https://github.com/Tudor-debug819/siteLayout",
       "liveLink": "https://github.com/Tudor-debug819/siteLayout/"
@@ -37,7 +37,7 @@ export class ProjectsComponent {
     {
       "title": "Currency Exchange",
       "description": "Currency exchange app to quickly calculate money conversion.",
-      "image": "imagini/currency.png",
+      "image": "assets/currency.png",
       "technologies": ["html", "css", "javascript"],
       "githubLink": "https://github.com/Tudor-debug819/convertor-valutar",
       "liveLink": "https://tudor-debug819.github.io/convertor-valutar/"
@@ -45,7 +45,7 @@ export class ProjectsComponent {
     {
       "title": "Random Name Generator",
       "description": "Helps you decide who is going to pay a bill at a restaurant by generating a random name.",
-      "image": "imagini/random-name.png",
+      "image": "assets/random-name.png",
       "technologies": ["html", "css", "javascript"],
       "githubLink": "https://github.com/Tudor-debug819/random-generator",
       "liveLink": "https://tudor-debug819.github.io/random-generator/"
@@ -53,7 +53,7 @@ export class ProjectsComponent {
     {
       "title": "Tip Calculator",
       "description": "Calculates how much tip you should give based on the meal price.",
-      "image": "imagini/bacsis.png",
+      "image": "assets/bacsis.png",
       "technologies": ["html", "css", "javascript"],
       "githubLink": "https://github.com/Tudor-debug819/bacsis",
       "liveLink": "https://tudor-debug819.github.io/bacsis/"
@@ -61,28 +61,10 @@ export class ProjectsComponent {
     {
       "title": "Multilevel Menu",
       "description": "Multilevel menu that transforms into a hamburger menu on smaller screens.",
-      "image": "imagini/multilevelmenu.png",
+      "image": "assets/multilevelmenu.png",
       "technologies": ["html", "css", "javascript"],
       "githubLink": "https://github.com/Tudor-debug819/multilevel-menu",
       "liveLink": "https://tudor-debug819.github.io/multilevel-menu/"
     },
-    {
-      title: 'Project 1',
-      description: 'Description for project 1.',
-      image: 'assets/project1.png',
-      link: 'https://example.com/project1',
-    },
-    {
-      title: 'Project 2',
-      description: 'Description for project 2.',
-      image: 'assets/project2.png',
-      link: 'https://example.com/project2',
-    },
-    {
-      title: 'Project 3',
-      description: 'Description for project 3.',
-      image: 'assets/project3.png',
-      link: 'https://example.com/project3',
-    }
   ];
 }
