f4f05dd5545d7379796c029102065abae7878dfa
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-11 10:11:09 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from "@angular/core";
 import{Routes, RouterModule} from '@angular/router';
+import { ProjectsComponent } from "./projects/projects.component";
+import { FormComponent } from "./form/form.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path:'',
+        component: ProjectsComponent
+    },
+    {
+        path:'contact',
+        component: FormComponent
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
