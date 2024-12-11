259335875bf49c8d9ebc412aaccffa4983a49e30
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-11 11:01:37 +0200

    adding routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { FooterComponent } from './footer/footer.component';
@@ -8,7 +8,7 @@ import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProjectsComponent, FooterComponent, FormComponent],
+  imports: [HeaderComponent, ProjectsComponent, FooterComponent, FormComponent ,RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
