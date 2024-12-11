64d43b4d97873260e3bbd869f130537ee7855360
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-11 11:53:02 +0200

    finish routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { FooterComponent } from './footer/footer.component';
@@ -8,7 +8,7 @@ import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProjectsComponent, FooterComponent, FormComponent ,RouterOutlet, RouterLink],
+  imports: [HeaderComponent, ProjectsComponent, FooterComponent, FormComponent ,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
