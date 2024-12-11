02e9c7957b9e3dc3638b1bbedbb84ff270bc7042
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2024-12-11 09:32:39 +0200

    adding footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { ProjectsComponent } from "./projects/projects.component";
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ProjectsComponent],
+  imports: [HeaderComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
